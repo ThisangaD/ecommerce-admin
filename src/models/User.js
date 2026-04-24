ae61901ab3fbae431f0ce81e97a0ffb3dba764d0
ThisangaD <thisanga.dinath@gmail.com>
2026-04-24 12:41:51 +0530

    feat(models): define User model and authentication schema

---
+++ src/models/User.js
@@ -0,0 +1,48 @@
+/**
+ * @file User.js
+ * @description Sequelize model for the `users` table.
+ */
+
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../config/database.js';
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(150),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM('admin', 'user'),
+      defaultValue: 'user',
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    modelName: 'User',
+    tableName: 'users',
+    timestamps: true,
+  }
+);
+
+export default User;
