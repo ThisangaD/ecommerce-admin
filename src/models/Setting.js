5d89a01e897d54ce2cceb715871ed43502741cf7
ThisangaD <thisanga.dinath@gmail.com>
2026-04-24 12:42:07 +0530

    feat(models): define Setting model for app configuration

---
+++ src/models/Setting.js
@@ -0,0 +1,26 @@
+/**
+ * @file Setting.js
+ * @description Sequelize model for the `settings` table.
+ */
+
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../config/database.js';
+
+class Setting extends Model {}
+
+Setting.init(
+  {
+    id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    key: { type: DataTypes.STRING(100), allowNull: false, unique: true },
+    value: { type: DataTypes.TEXT, allowNull: true },
+    description: { type: DataTypes.STRING(255), allowNull: true },
+  },
+  {
+    sequelize,
+    modelName: 'Setting',
+    tableName: 'settings',
+    timestamps: true,
+  }
+);
+
+export default Setting;
